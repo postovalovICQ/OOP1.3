3b1ad011a2ef5a4a34f22fee58c8b24de91aab97
postovalovICQ <71002789+postovalovICQ@users.noreply.github.com>
2020-09-20 16:00:47 +0500

    Add files via upload

---
+++ oop1.3/oop1.3/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace oop1._3
+{
+    class zoo
+    {
+        public string Animal;
+        public string Sex;
+        public string Name;
+        internal int Price;
+        internal int Amount;
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Зоомагазин");
+            zoo zoo = new zoo();
+
+            Console.Write("Введите животное: ");
+            zoo.Animal = Convert.ToString(Console.ReadLine());
+
+            Console.Write("Введите пол: ");
+            zoo.Sex = Convert.ToString(Console.ReadLine());
+
+            Console.Write("Введите кличку: ");
+            zoo.Name = Convert.ToString(Console.ReadLine());
+
+            Console.Write("Введите цену: ");
+            zoo.Price = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Введите количество: ");
+            zoo.Amount = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("---------------");
+            Console.WriteLine("---------------");
+            Console.WriteLine(zoo.Animal);
+            Console.WriteLine(zoo.Sex);
+            Console.WriteLine(zoo.Name);
+            Console.WriteLine(zoo.Price);
+            Console.WriteLine(zoo.Amount);
+            Console.WriteLine("---------------");
+            Console.ReadLine();
+        }
+    }
+}
